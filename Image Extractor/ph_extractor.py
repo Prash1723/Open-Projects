ce1418aa281588e4f98071c8850dbb86297c2ccc
Prash1723 <s.praveen795@gmail.com>
2023-08-04 15:19:42 +0530

    Functions

--- Image Extractor/ph_extractor.py
+++ Image Extractor/ph_extractor.py
@@ -2,52 +2,60 @@ from PIL import Image
 import os, pyperclip, re, send2trash
 from pytesseract import image_to_string
 from Screenshot import Screenshot_Clipping
+from rich.console import Console
+
+rc = Console()
 
 # Assign Path
 path = os.path.dirname(os.path.realpath(__file__))
 input_path = path + '/Input/'
 
-# Open Image
-all_text = []
+def open_img():
+    """Open Screenshot"""
+    # Open Image
+    all_text = []
 
-for root, dirs, filenames in os.walk(input_path):
-    for filename in filenames:
-        try:
-            img = Image.open(input_path + filename)
-            all_text.append(image_to_string(img))
-            # Deleting the files scanned
-            send2trash.send2trash(input_path + filename)
-        except:
-            continue
+    for root, dirs, filenames in os.walk(input_path):
+        for filename in filenames:
+            try:
+                img = Image.open(input_path + filename)
+                all_text.append(image_to_string(img))
+                # Deleting the files scanned
+                send2trash.send2trash(input_path + filename)
+            except:
+                continue
 
-# Regex code
-phone_regex = re.compile(r'''(
-    (\+91|0)?                                   # country code
-    (\s|-|\.)?                                  # seperator
-    (\d{5})                                     # 5 digits
-    (\s|-|\.)?                                  # seperator
-    (\d{5})                                     # 5 digits
-)''', re.VERBOSE)
+    text = str('\n'.join(all_text))
+    return text
 
-text = str('\n'.join(all_text))
+def number_parse(text):
+    """Parse phone number from the screenshot"""
+    # Regex code
+    phone_regex = re.compile(r'''(
+        (\+91|0)?                                   # country code
+        (\s|-|\.)?                                  # seperator
+        (\d{5})                                     # 5 digits
+        (\s|-|\.)?                                  # seperator
+        (\d{5})                                     # 5 digits
+    )''', re.VERBOSE)
 
-matches = []
-phone_num = ''
+    matches = []
+    phone_num = ''
 
-# Extract and Arrange Phone Numbers
-for groups in phone_regex.findall(text):
-    phone_num = ''.join([groups[3], groups[5]])
-    matches.append(phone_num)
+    # Extract and Arrange Phone Numbers
+    for groups in phone_regex.findall(text):
+        phone_num = ''.join([groups[3], groups[5]])
+        matches.append(phone_num)
 
-if len(matches) > 0:
-    distinct_matches = list(dict.fromkeys(matches))
+    if len(matches) > 0:
+        distinct_matches = list(dict.fromkeys(matches))
 
-    if len(matches)!=len(distinct_matches):
-        print(str(len(matches)-len(distinct_matches)) + ' Duplicates Removed')
+        if len(matches)!=len(distinct_matches):
+            rc.log(str(len(matches) - len(distinct_matches)) + ' Duplicates Removed')
+        else:
+            rc.log('No duplicates found!')
+
+        pyperclip.copy('\n'.join(distinct_matches))
+        rc.log('CopieD to clipboard')
     else:
-        print('No duplicates found!')
-
-    pyperclip.copy('\n'.join(distinct_matches))
-    print('Copied to clipboard')
-else:
-    print('No Phone no. was found!!')
+        rc.log('No Phone no. was found!!')
